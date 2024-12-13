daae630fb8a21b25cd45c090a14c57d4ad6f3252
Nazanin <nazaninbakhshi386@gmail.com>
2024-12-13 17:42:46 +0330

    Implement the homepage

--- header.php
+++ header.php
@@ -13,22 +13,14 @@
 <body <?php body_class('flex flex-col') ?> >
     <?php wp_body_open(); ?>
 
-    <header class="px-4">
+    <header class="w-full px-4 pt-2">
         <div class="flex max-w-screen-xl mx-auto h-[70px]">
-            <div class="flex-none  pl-1">
-                <a class="relative z-2" href="<?php echo home_url() ?>">
-                    <?php the_custom_logo() ?>
-                </a>
+            <div class="pl-3">
+                <?php the_custom_logo() ?>
             </div>
-            <div id="menu" class="flex-1 pt-1">
-                <button class="menu-icon block md:hidden mt-6" onclick="jQuery('#menu').toggleClass('open')"></button>
-                <div id="menu-items" class="pt-5 hidden md:block text-center">
-                    <?= wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-                </div>
-            </div>
-            <div class="flex-none pt-2" id="search-container">
-                
-            </div>
+            <div id="menu-items" class="grow pt-5">
+                <?= wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+            </div>                
         </div>
     </header>
 
